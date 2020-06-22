f1d9efd36711c42922cf7081d7ee10d65d5c60f4
Md Nayeem <codernayeem898@gmail.com>
2020-06-22 15:30:20 +0600

    Added map route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint, render_template, redirect, request, session, Response, url_for
 from colorama import init, Fore, Back
 import socket, sys, config as cfg
-from tools import AppData, get_folders
+from tools import AppData, get_folders, join
 
 init()
 app = Flask(__name__, instance_relative_config=False, static_folder='.static', template_folder='.templates')
@@ -13,7 +13,16 @@ def index_view():
     folders = sorted(get_folders(appData.get('rec_folder')))
     return render_template('index.html', rec_names=folders, count=len(folders))
 
-	
+@app.route('/map/<rec_name>/')
+def map_go_view(rec_name):
+    global appData
+    rec_folder = appData.get('rec_folder')
+    if not rec_name or not rec_name in get_folders(rec_folder):
+        return redirect('/')
+    
+    all_dates = sorted(get_folders(join(rec_folder, rec_name)))
+    return render_template('map.html', rec_name=rec_name, all_dates=all_dates, count=len(all_dates))
+
 if __name__ == "__main__":
     host = socket.gethostbyname(socket.gethostname())
     port = 1999
