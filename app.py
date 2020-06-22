617286f8023e2aa71fb098c1562a7e527dd516dd
Md Nayeem <codernayeem898@gmail.com>
2020-06-22 22:31:09 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint, render_template, redirect, request, session, Response, url_for
 from colorama import init, Fore, Back
 import socket, sys, config as cfg
-from tools import AppData, get_folders, join
+from tools import AppData, get_folders, join, sizeSince
 
 init()
 app = Flask(__name__, instance_relative_config=False, static_folder='.static', template_folder='.templates')
@@ -14,13 +14,31 @@ def index_view():
     return render_template('index.html', rec_names=folders, count=len(folders))
 
 @app.route('/map/<rec_name>/')
-def map_go_view(rec_name):
+@app.route('/map/<rec_name>/<rec_date>')
+def map_go_view(rec_name, rec_date=None):
     global appData
     rec_folder = appData.get('rec_folder')
     if not rec_name or not rec_name in get_folders(rec_folder):
         return redirect('/')
     
     all_dates = sorted(get_folders(join(rec_folder, rec_name)))
+
+    if rec_date:
+        if rec_date in all_dates:
+            date_index = all_dates.index(rec_date)
+            old_date, next_date = None, None
+            if date_index != 0:
+                old_date = all_dates[date_index-1]
+            if date_index != len(all_dates) - 1:
+                next_date = all_dates[date_index+1]
+
+            all_records = appData.get_records(rec_name, rec_date)
+            all_records.sort(key=lambda i: i['call_time_object'], reverse=False)
+
+            return render_template('records.html', rec_name=rec_name, size=appData.get_files_size(all_records), records=all_records, total=len(all_records), date=rec_date, old_date=old_date, next_date=next_date)
+        else:
+            return redirect('/map/'+rec_name)
+
     return render_template('map.html', rec_name=rec_name, all_dates=all_dates, count=len(all_dates))
 
 if __name__ == "__main__":
